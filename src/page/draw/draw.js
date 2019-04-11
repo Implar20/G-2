a98d07bf8fce852d2e1f5e2d453867c3d9c97214
implar <implar20@gmail.com>
2019-04-11 17:18:56 +0800

    draw_aside

--- src/page/draw/draw.js
+++ src/page/draw/draw.js
@@ -1,9 +1,8 @@
-$(function() {
+$(function () {
     /* 滚动条插件 */
-    $(window).on("load",function(){
+    $(window).on("load", function () {
         $(".content_icon").mCustomScrollbar()
     })
-
 
     var windowW = $(window).width()
     var windowH = $(window).height()
@@ -12,5 +11,76 @@ $(function() {
     $('.section').css({
         'width': windowW - 410,
         'height': windowH - 60
+    })
+    /**
+     * fabric.js 控制
+     */
+    var ctx = new fabric.Canvas('ctx')
+    function fabricShow(path) {
+        fabric.Image.fromURL(path, function (oImg) {
+            oImg.set({
+                top: 297,
+                left: 297,
+                originX: 'center',
+                originY: 'center'
+            })
+            ctx.add(oImg)
+        })
+    }
+    $('.icon_item img').click(function () {
+        var path = $(this).attr('src')
+        fabricShow(path)
+    })
+    $('.shape_item img').click(function () {
+        var path = $(this).attr('src')
+        fabricShow(path)
+    })
+
+    /**
+     * 预览 保存 下载
+     */
+
+    /**
+     * 切换 tag
+     */
+    function switchTag(ele) {
+        if (ele.attr('class').indexOf('active') !== -1)
+            return
+        ele.addClass('active').siblings().removeClass('active')
+    }
+
+    $('.icon').click(function () {
+        switchTag($(this))
+        $('.list_menu').css('display', 'inline-block')
+        $('.icon_item').css('display', 'none')
+        $('.shape_item').css('display', 'inline-block')
+    })
+    $('.shape').click(function () {
+        switchTag($(this))
+        $('.list_menu').css('display', 'inline-block')
+        $('.shape_item').css('display', 'none')
+        $('.icon_item').css('display', 'inline-block')
+    })
+    $('.text').click(function () {
+        switchTag($(this))
+        $('.list_menu').css('display', 'none')
+        $('.text_menu').css('display', 'inline-block')
+    })
+    $('.bg').click(function () {
+        switchTag($(this))
+        $('.list_menu').css('display', 'none')
+    })
+
+    /**
+     * fabric 文字
+     */
+    $('.text_menu h1').click(function() {
+        var str = $('.text_menu input').val()
+        var text = new fabric.Text(str, {
+            left: 100,
+            top: 100,
+            fontWeight: 'bold'
+        })
+        ctx.add(text)
     })
 })
